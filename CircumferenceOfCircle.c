052bf01489e89c829ec9c87a5eaa59b5e3154839
WebAishwarya <140585542+WebAishwarya@users.noreply.github.com>
2024-10-19 00:23:21 +0530

    Create CircumferenceOfCircle.c

---
+++ CircumferenceOfCircle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    float radius = 6.15;
+    float CircumferenceOfCircle;
+    
+    CircumferenceOfCircle = 2 * 3.14 * radius;
+    
+    printf("Value of the radius (r) is : %.2f\n\n", radius);
+    printf("Circumference Of The Circle : %.2f\n", CircumferenceOfCircle);
+    
+    return 0;
+}
